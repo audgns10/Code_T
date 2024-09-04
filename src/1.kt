a2def8784d22159c3fe1c4fa07214b1d97b9f872
명훈 <s23065@gsm.hs.kr>
2024-09-04 20:47:06 +0900

    :sparkles: :: first num

---
+++ src/1.kt
@@ -0,0 +1,11 @@
+// 첫번째 문제
+// 문자열 str이 주어질 때, str을 출력하는 코드를 작성해 보세요.
+
+fun main(args: Array<String>) {
+    val s1 = readLine()!!
+    println(s1)
+} // 나의 답
+
+fun other(args: Array<String>) {
+    readLine()?.let(::println)
+} // 다른 사람의 답변
