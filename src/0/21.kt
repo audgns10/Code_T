bc4526bd6dcdbc788c346a9bd05bf2f12ffa2a71
명훈 <s23065@gsm.hs.kr>
2024-11-25 08:44:14 +0900

    :sparkles: :: Resolve 21.kt(num_21)

---
+++ src/0/21.kt
@@ -0,0 +1,11 @@
+// 21.문제
+
+// 숫자로만 이루어진 문자열 n_str이 주어질 때, n_str을 정수로 변환하여 return하도록 solution 함수를 완성해주세요.
+
+class Solution {
+    fun solution(n_str: String): Int = n_str.toInt()
+} // 나의 풀이
+
+// n_str 변수의 값을 String 타입에서 Int 타입으로만 바꾸어주면 되기 때문에 .toInt() 함수를 사용하여 변환해줌
+
+// 다른 사람의 풀이도 똑같음
