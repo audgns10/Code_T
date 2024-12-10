7e93448ccf26dfc7a94e589ea6065ff4a32d098c
명훈 <s23065@gsm.hs.kr>
2024-12-10 11:28:39 +0900

    :memo: :: Add Import Code

--- src/baekjoon/25.kt
+++ src/baekjoon/25.kt
@@ -4,6 +4,9 @@
 // 첫째 줄에 수의 개수 N(1 ≤ N ≤ 1,000,000)이 주어진다. 둘째 줄부터 N개의 줄에는 숫자가 주어진다. 이 수는 절댓값이 1,000,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.
 // 첫째 줄부터 N개의 줄에 내림차순으로 정렬한 결과를 한 줄에 하나씩 출력한다.
 
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 class Solution {
     fun solution(list: List<Int>) : List<Int> {
         return list.sortedDescending()
